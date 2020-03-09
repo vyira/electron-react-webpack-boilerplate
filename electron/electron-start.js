538b52d9e806ee3f3adbd982341cd3bf589477ed
Awatansa Vishwakarma <ravi.awatansa@gmail.com>
2020-03-09 06:03:03 +0530

    temp

--- electron/electron-start.js
+++ electron/electron-start.js
@@ -1,6 +1,6 @@
 const { BrowserWindow, ipcMain, Menu, app } = require('electron')
 const path = require('path')
-const isDev = require('electron-is-dev')
+const isDev = true
 
 let win
 const createWindow = () => {
@@ -10,15 +10,16 @@ const createWindow = () => {
         },
         title: "Electron React Boiler-plate"
     })
-    win.loadURL(isDev ? 'http://localhost:9000' : `file://${path.join(__dirname, '..', 'build', 'index.html')}`)
-    if(isDev){
-        win.webContents.openDevTools({mode:"detach"})
-    }
+    // win.loadURL(isDev ? 'http://localhost:9000' : `file://${path.join(__dirname, '..', 'build', 'index.html')}`)
+    win.loadURL(`file://${path.join(__dirname, '..', 'build', 'index.html')}`)
+
+    win.webContents.openDevTools({ mode: "detach" })
+
 }
 
 app.on('ready', createWindow)
 
-ipcMain.on('event',function(e){
+ipcMain.on('event', function (e) {
     console.log('Event Triggered')
     e.returnValue('Accepted')
 })
